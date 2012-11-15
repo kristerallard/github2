679bb99977e80ffe0092ca5e4ee7ca158bca3990
lorenanerol <lkandersson@hotmail.com>
2012-11-15 09:45:16 +0100

    blaj

---
+++ github2/src/Github2.java
@@ -0,0 +1,14 @@
+
+public class Github2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Jag är ett skittråkigt githubtest som inte gör något");
+		
+	}
+
+}
